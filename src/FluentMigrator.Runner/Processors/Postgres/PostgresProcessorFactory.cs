5005ce6ab18cd02a06480addd5d327b221fd8153
Rudis Muiznieks <rudis@sitosis.com>
2014-11-20 09:51:35 -0600

    updated with pgpass file support

--- src/FluentMigrator.Runner/Processors/Postgres/PostgresProcessorFactory.cs
+++ src/FluentMigrator.Runner/Processors/Postgres/PostgresProcessorFactory.cs
@@ -1,13 +1,29 @@
 ﻿namespace FluentMigrator.Runner.Processors.Postgres
 {
+    using System;
     using Generators.Postgres;
+    using Pgpass.Net;
 
     public class PostgresProcessorFactory : MigrationProcessorFactory
     {
+        private static string GetPgConnectionString(string connstr)
+        {
+            var parts = connstr.Substring(7).Split(';');
+            if (parts.Length != 3)
+                throw new FormatException("Bad pgpass connection string (should be pgpass=server:port;database;user)");
+            var pgpass = new Pgpass(parts[0]);
+            return pgpass.GetConnectionString(parts[1], parts[2]);
+        }
+
         public override IMigrationProcessor Create(string connectionString, IAnnouncer announcer, IMigrationProcessorOptions options)
         {
             var factory = new PostgresDbFactory();
-            var connection = factory.CreateConnection(connectionString);
+
+            var connection =
+                factory.CreateConnection(connectionString.StartsWith("pgpass=", StringComparison.OrdinalIgnoreCase)
+                    ? GetPgConnectionString(connectionString)
+                    : connectionString);
+
             return new PostgresProcessor(connection,
                 new PostgresGenerator(LowerCaseIdentifiers(options.ProviderSwitches)), announcer, options, factory);
         }
